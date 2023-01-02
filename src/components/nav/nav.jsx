cf3375d198dd57c46083dc5231794bb97428327a
| Ewa Szyszka | エーワー シズカ | 에바 스스카 | <79250896+ESzyszka@users.noreply.github.com>
2023-01-02 22:24:43 +0900

    Add files via upload

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './nav.css'
+import Profile from '/Users/ewa.szyszka/Desktop/react_portfolio/react_portfolio/src/assets/ono.png';
+
 
 const Nav = () => {
 
   return(
-    <div>Nav</div>
+    <div>
+      <img src={Profile} style={{ width:700, marginRight: 30, }} alt= ""/>
+    </div>
+
   )
 }
 
